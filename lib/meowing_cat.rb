3602e2108437eaca0cc29af317c7311d92838c8a
Jordan Harris <jharris@wework.com>
2018-07-11 16:06:40 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,25 @@
 ## code your solution here. 
+class Cat
+  def name=(fido)
+    @name = fido
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(beagle)
+    @breed = beagle
+  end
+
+  def breed
+    @breed
+  end
+end
+
+
+fido = Dog.new
+fido.name = "Fido"
+
+snoopy = Dog.new
+snoopy.breed = "Beagle" 
