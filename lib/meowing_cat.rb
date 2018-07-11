fee2d9757e4a0109a77cbb96f9f06b3c369f9967
Jordan Harris <jharris@wework.com>
2018-07-11 16:08:09 +0000

    automatically backed up by learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,36 +1,12 @@
 ## code your solution here. 
 class Cat
-  def name=(fido)
-    @name = maRU
+  def name=(maru)
+    @name = maru
   end
 
   def name
     @name
   end
-
-  def breed=(beagle)
-    @breed = beagle
-  end
-
-  def breed
-    @breed
-  end
 end
 
 
-fido = Dog.new
-fido.name = "Fido"
-
-snoopy = Dog.new
-snoopy.breed = "Beagle" 
-
-
-maru = Cat.new
-maru.name = "Maru"
-
-maru.name
-# => "Maru"
-
-maru.meow
-# "meow!"
-# => nil
